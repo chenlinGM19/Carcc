9be039839e8b8fd1157d2c6e44b44b80497e1780
chenlinGM19 <60935905+chenlinGM19@users.noreply.github.com>
2026-01-29 00:32:13 +0800

    Update MainActivity.java

--- android/app/src/main/java/com/carlyrix/app/MainActivity.java
+++ android/app/src/main/java/com/carlyrix/app/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
